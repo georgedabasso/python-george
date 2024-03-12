f07ec4c6285fa19b4d71e795156e4aef7fdfeae1
nellymusyawa99 <nellymusyawa@gmail.com>
2024-03-12 06:33:46 -0400

    3rd update

--- GEORGE'S_python_intro.py
+++ GEORGE'S_python_intro.py
@@ -6,9 +6,9 @@ this is a simple multiline comment in python
 #variables
 student_name = "Jonathan" #data type is string
 student_age = 25 #integer
-student_fee= 100.0 #float
+student_fee = 100.0 #float
 student_marks = 100 #integer
-is_male= True #boolean
+is_male = True #boolean
 
 #outputting values in variables
 print(student_name, student_age, student_fee)
@@ -36,3 +36,23 @@ _book_price=20.0 #valid variable name
 
 x=y=z=10 #assigning one value to multiple variables
 x,y,z= 30, 40, 50 #multiple values being assigned multiple variables
+
+price= 10
+qty= 4
+total_price= price*qty
+print("the total price is: " +str(total_price)+ "Kenyan shillings")
+
+firstname= "Jonathan"
+secondname= "Kimani"
+thirdname= firstname*4
+print("my thirdname is :" ,thirdname,)
+
+#logical operator
+constituency= "embakasi"
+
+if constituency=="embakasi" or "westlands" or "kasarani":
+    print("can be governor")
+else:
+    print("cannot be governor")
+
+
